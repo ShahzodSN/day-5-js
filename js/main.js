8b4fcdfb1891e1461769854d1907d1bbb099603d
shahzod <shahzodnematov33@gmail.com>
2022-04-03 20:26:35 +0500

    Add js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let age = Math.random()*100;
+console.log(age);
+
+if (age > 1 && age <= 17) {
+
+    console.log("Maktab yoshi");
+
+} else if (age >= 18 && age <= 24){
+
+    console.log("Universitet yoshi");
+
+} else if (age >= 25 && age <= 40){
+
+    console.log("Ishlash yoshi");
+
+} else if (age >= 41) {
+
+    console.log("Nuroniylar yoshi");
+
+}
